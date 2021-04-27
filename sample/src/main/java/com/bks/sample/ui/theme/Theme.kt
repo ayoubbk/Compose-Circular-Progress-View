12a05dbea8315f805b1f5a771123201ec4a428a5
ayoub.benkhemis <ayoub.benkhemis@gmail.com>
2021-04-27 13:52:12 +0200

    update compose version, add project dependencies

--- sample/src/main/java/com/bks/sample/ui/theme/Theme.kt
+++ sample/src/main/java/com/bks/sample/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun CircularProgressViewTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
+fun CircularProgressViewTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
